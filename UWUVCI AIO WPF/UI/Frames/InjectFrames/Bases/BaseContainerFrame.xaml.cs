dac0f387fd48978bdfb1a8133f5aee4611516264
ZestyTS <ZestyTS@gmail.com>
2020-10-01 23:42:32 -0700

    Simplified using a ternary

--- UWUVCI AIO WPF/UI/Frames/InjectFrames/Bases/BaseContainerFrame.xaml.cs
+++ UWUVCI AIO WPF/UI/Frames/InjectFrames/Bases/BaseContainerFrame.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using GameBaseClassLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using UWUVCI_AIO_WPF.Classes;
 
 
 namespace UWUVCI_AIO_WPF.UI.Frames.InjectFrames.Bases
@@ -30,34 +18,31 @@ namespace UWUVCI_AIO_WPF.UI.Frames.InjectFrames.Bases
         {
             InitializeComponent();
             
-           
-
             mvm = (MainViewModel)FindResource("mvm");
             
             mvm.GameConfiguration.Console = console;
-            if (console == GameConsoles.GCN) this.console = GameConsoles.WII;
-            else this.console = console;
+            
+            this.console = console == GameConsoles.GCN ? GameConsoles.WII : console;
+
             mvm.GetBases(this.console);
             mvm.bcf = this;
             if (console == GameConsoles.NES)
             {
                 cbCombo.ToolTip = "We recommend Metal Slader Glory for NES Injection!";
             }
-            if (console == GameConsoles.GBA)
+            else if (console == GameConsoles.GBA)
             {
                 cbCombo.ToolTip = "For RTC use RockMan EXE 4.5 as base";
             }
-            if (console == GameConsoles.SNES)
+            else if (console == GameConsoles.SNES)
             {
                 cbCombo.ToolTip = "We recommend Kirby's Dream Land 3 for SNES Injection!";
             }
-            if(console == GameConsoles.MSX)
+            else if(console == GameConsoles.MSX)
             {
                 cbCombo.ToolTip = "You need to try which base works best.\nNemesis has the best MSX compatibility and Goemon the best MSX2. \nBoth support MSX and MSX2 ROMs";
             }
         }
-
-       
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -67,42 +52,32 @@ namespace UWUVCI_AIO_WPF.UI.Frames.InjectFrames.Bases
                 mvm.removeCBASE();
                 if (cbCombo.SelectedIndex != -1)
                 {
-                    
-                        if (cbCombo.SelectedIndex == 0)
+                    if (cbCombo.SelectedIndex == 0)
 
-                        {
-                            id.Visibility = Visibility.Hidden;
-                            idtxt.Visibility = Visibility.Hidden;
-                            fLoadFrame.Content = new CustomBaseFrame(mvm.LBases[cbCombo.SelectedIndex], console, insertedConfig);
-                        }
-                        else
-                        {
-                            id.Visibility = Visibility.Visible;
+                    {
+                        id.Visibility = Visibility.Hidden;
+                        idtxt.Visibility = Visibility.Hidden;
+                        fLoadFrame.Content = new CustomBaseFrame(mvm.LBases[cbCombo.SelectedIndex], console, insertedConfig);
+                    }
+                    else
+                    {
+                        id.Visibility = Visibility.Visible;
 
-                            idtxt.Visibility = Visibility.Visible;
-                            fLoadFrame.Content = new NonCustomBaseFrame(mvm.LBases[cbCombo.SelectedIndex], console, insertedConfig, this);
-                            idtxt.Content = mvm.GameConfiguration.BaseRom.Tid;
-                        }
-                        mvm.OldIndex = cbCombo.SelectedIndex;
-                   
-                           
-                        
-                        
-                    
-                    
+                        idtxt.Visibility = Visibility.Visible;
+                        fLoadFrame.Content = new NonCustomBaseFrame(mvm.LBases[cbCombo.SelectedIndex], console, insertedConfig, this);
+                        idtxt.Content = mvm.GameConfiguration.BaseRom.Tid;
+                    }
+                    mvm.OldIndex = cbCombo.SelectedIndex;
                 }
             }
             catch (Exception)
             {
-
-            }
-            
-          
+                //left blank on purpose
+            } 
         }
 
         private void id_Click(object sender, RoutedEventArgs e)
         {
-            
             Clipboard.SetText(mvm.GameConfiguration.BaseRom.Tid);
         }
     }
