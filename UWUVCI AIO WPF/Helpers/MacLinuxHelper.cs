b95eda5b3da06492e59f76373af36cfb737d1f4e
ZestyTS <ZestyTS@gmail.com>
2024-10-08 22:48:38 -0700

    Added the beginning of what's required for the middle-man app for Mac/Linux

---
+++ UWUVCI AIO WPF/Helpers/MacLinuxHelper.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UWUVCI_AIO_WPF.Models;
+
+namespace UWUVCI_AIO_WPF.Helpers
+{
+    public class MacLinuxHelper
+    {
+        public static void WriteFailedStepToJson(string functionName, string toolName, string arguments, string currentDirectory = "")
+        {
+            // Get the base directory where the application is running
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string toolsJsonPath = Path.Combine(basePath, "tools.json");
+
+            if (string.IsNullOrEmpty(currentDirectory))
+                currentDirectory = basePath;
+
+            var step = new ToolStep
+            {
+                ToolName = toolName,
+                Arguments = arguments,
+                CurrentDirectory = currentDirectory,
+                Function = functionName
+            };
+
+            List<ToolStep> steps;
+
+            if (File.Exists(toolsJsonPath))
+                steps = JsonConvert.DeserializeObject<List<ToolStep>>(File.ReadAllText(toolsJsonPath)) ?? new List<ToolStep>();
+            else
+                steps = new List<ToolStep>();
+
+            steps.Add(step);
+            File.WriteAllText(toolsJsonPath, JsonConvert.SerializeObject(steps));
+        }
+    }
+}
---
+++ UWUVCI AIO WPF/Models/ToolStep.cs
@@ -0,0 +1,11 @@
+﻿namespace UWUVCI_AIO_WPF.Models
+{
+    public class ToolStep
+    {
+        public string ToolName { get; set; } // The name of the tool to run (e.g., wit, nfs2iso2nfs)
+        public string Arguments { get; set; } // The arguments to pass to the tool
+        public string CurrentDirectory { get; set; } // The directory where the tool should run
+        public string Function { get; set; } // The function in UWUVCI where the step originated
+    }
+
+}
