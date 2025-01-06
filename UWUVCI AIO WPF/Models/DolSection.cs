a316265fcb5a862192f9f62d5f47adf4347965fa
ZestyTS <ZestyTS@gmail.com>
2025-01-05 19:21:35 -0800

    Added checks to the dolheader

--- UWUVCI AIO WPF/Models/DolSection.cs
+++ UWUVCI AIO WPF/Models/DolSection.cs
@@ -9,13 +9,20 @@ namespace UWUVCI_AIO_WPF.Models
         public uint FileOffset { get; set; }
         public uint Size { get; set; }
 
-        public List<DolSection> ReadDolHeader(string dolFilePath)
+        public static List<DolSection> ReadDolHeader(string dolFilePath)
         {
             var sections = new List<DolSection>();
+
+            if (!File.Exists(dolFilePath))
+                throw new FileNotFoundException($"The file '{dolFilePath}' was not found.");
+
             using (FileStream fs = new FileStream(dolFilePath, FileMode.Open, FileAccess.Read))
             using (BinaryReader br = new BinaryReader(fs))
             {
-                fs.Seek(0x00, SeekOrigin.Begin); // Start of DOL file
+                if (fs.Length < 0x100) // Minimum header size
+                    throw new InvalidDataException("The file is too small to be a valid DOL file.");
+
+                fs.Seek(0x00, SeekOrigin.Begin); // Start of DOL file header
 
                 for (int i = 0; i < 7; i++) // Text sections
                 {
@@ -23,12 +30,7 @@ namespace UWUVCI_AIO_WPF.Models
                     uint addr = br.ReadUInt32();
                     uint size = br.ReadUInt32();
                     if (size > 0)
-                        sections.Add(new DolSection
-                        {
-                            FileOffset = offset,
-                            MemoryAddress = addr,
-                            Size = size
-                        });
+                        sections.Add(new DolSection { FileOffset = offset, MemoryAddress = addr, Size = size });
                 }
 
                 for (int i = 0; i < 11; i++) // Data sections
@@ -37,15 +39,12 @@ namespace UWUVCI_AIO_WPF.Models
                     uint addr = br.ReadUInt32();
                     uint size = br.ReadUInt32();
                     if (size > 0)
-                        sections.Add(new DolSection
-                        {
-                            FileOffset = offset,
-                            MemoryAddress = addr,
-                            Size = size
-                        });
+                        sections.Add(new DolSection { FileOffset = offset, MemoryAddress = addr, Size = size });
                 }
             }
+
             return sections;
         }
     }
+
 }
