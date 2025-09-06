435081aad8312c6e8c1e4b8852ef9066ad6766b8
ZestyTS <ZestyTS@gmail.com>
2025-09-06 11:09:02 -0700

    Rename FileUtils to FileHelpers and add MoveOverwrite
    Renamed FileUtils.cs to FileHelpers.cs and updated the class name accordingly. Added a new MoveOverwrite method to move files and overwrite the destination if it exists.

--- UWUVCI AIO WPF/Helpers/FileHelpers.cs
+++ UWUVCI AIO WPF/Helpers/FileHelpers.cs
@@ -1,6 +1,8 @@
-﻿namespace UWUVCI_AIO_WPF.Helpers
+﻿using System.IO;
+
+namespace UWUVCI_AIO_WPF.Helpers
 {
-    public static class FileUtils
+    public static class FileHelpers
     {
         public static int FindFreeSpace(byte[] fileData, int requiredSize)
         {
@@ -26,6 +28,13 @@
 
             Logger.Log("No free space found in the file.");
             return -1; // No free space found
+        }
+        public static void MoveOverwrite(string source, string destination)
+        {
+            if (File.Exists(destination))
+                File.Delete(destination);
+
+            File.Move(source, destination);
         }
     }
 }
