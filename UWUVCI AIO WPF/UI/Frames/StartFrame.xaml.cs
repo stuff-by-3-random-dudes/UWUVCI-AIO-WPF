cc91a2c132bc99791a9e221d90442c633d6e004b
ZestyTS <ZestyTS@gmail.com>
2025-10-21 21:02:02 -0700

    Fixed wording for ko-fi

--- UWUVCI AIO WPF/UI/Frames/StartFrame.xaml.cs
+++ UWUVCI AIO WPF/UI/Frames/StartFrame.xaml.cs
@@ -17,8 +17,7 @@ namespace UWUVCI_AIO_WPF.UI.Frames
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //Process.Start("https://ko-fi.com/nicoaicp");
-            Button_Click2(sender, e);
+            Process.Start("https://ko-fi.com/nicoaicp");
         }
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
