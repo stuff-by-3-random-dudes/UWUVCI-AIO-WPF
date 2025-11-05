e8218a32be7220a6056b5a578b9a938e2b54da38
ZestyTS <ZestyTS@gmail.com>
2025-11-04 20:22:51 -0800

    Rotate AES install key parts (2025-11-04 20:22:51)

--- UWUVCI AIO WPF/Services/LocalInstallGuard.cs
+++ UWUVCI AIO WPF/Services/LocalInstallGuard.cs
@@ -208,10 +208,10 @@ namespace UWUVCI_AIO_WPF.Services
         private static byte[] BuildAesKey()
         {
             // Short split parts (rotated per release by build script or manually)
-            string p1 = "bHP";
-            string p2 = "VPL";
-            string p3 = "Icp";
-            string p4 = "F50";
+            string p1 = "mZL";
+            string p2 = "vhA";
+            string p3 = "16p";
+            string p4 = "4J5";
             var combined = p1 + p2 + p3 + p4;
             var key = Encoding.UTF8.GetBytes(combined);
 
@@ -232,6 +232,7 @@ namespace UWUVCI_AIO_WPF.Services
         }
     }
 }
+
 
 
 
